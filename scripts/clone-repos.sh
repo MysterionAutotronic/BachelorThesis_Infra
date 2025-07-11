2075c27f35feeef92af72b1717e407f4314bd9f1
David Linhardt <divad2012@web.de>
2025-07-11 11:08:55 +0200

    add script to clone all necessary repositories for the project

---
+++ scripts/clone-repos.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/MysterionAutotronic/BachelorThesis_TenantFE.git
+git clone https://github.com/MysterionAutotronic/BachelorThesis_TenantBE.git
+git clone https://github.com/MysterionAutotronic/BachelorThesis_DashboardFE.git
+git clone https://github.com/MysterionAutotronic/BachelorThesis_DashboardBE.git
