2bc76057630c9074b1f6925910d81d7b4db38051
David Linhardt <divad2012@web.de>
2025-07-21 16:31:43 +0200

    refine clone repos script

--- scripts/clone-repos.sh
+++ scripts/clone-repos.sh
@@ -1,4 +1,27 @@
-git clone https://github.com/MysterionAutotronic/BachelorThesis_TenantFE.git
-git clone https://github.com/MysterionAutotronic/BachelorThesis_TenantBE.git
-git clone https://github.com/MysterionAutotronic/BachelorThesis_DashboardFE.git
-git clone https://github.com/MysterionAutotronic/BachelorThesis_DashboardBE.git
+if [ ! -d "BachelorThesis_TenantFE" ]; then
+    echo "Cloning Tenant Frontend repository..."
+    git clone https://github.com/MysterionAutotronic/BachelorThesis_TenantFE.git
+else
+    echo "Tenant Frontend repository already exists."
+fi
+
+if [ ! -d "BachelorThesis_TenantBE" ]; then
+    echo "Cloning Tenant Backend repository..."
+    git clone https://github.com/MysterionAutotronic/BachelorThesis_TenantBE.git
+else
+    echo "Tenant Backend repository already exists."
+fi
+
+if [ ! -d "BachelorThesis_DashboardFE" ]; then
+    echo "Cloning Dashboard Frontend repository..."
+    git clone https://github.com/MysterionAutotronic/BachelorThesis_DashboardFE.git
+else
+    echo "Dashboard Frontend repository already exists."
+fi
+
+if [ ! -d "BachelorThesis_DashboardBE" ]; then
+    echo "Cloning Dashboard Backend repository..."
+    git clone https://github.com/MysterionAutotronic/BachelorThesis_DashboardBE.git
+else
+    echo "Dashboard Backend repository already exists."
+fi
