ca13d7f57ce011bf40b27b38e9a13d361a604af0
David Linhardt <divad2012@web.de>
2025-07-22 16:40:47 +0200

    WIP create dashboard workspace

--- scripts/create-dashboard-workspace.sh
+++ scripts/create-dashboard-workspace.sh
@@ -7,29 +7,43 @@ export PATH="$HOME/Dokumente/BachelorThesis_Infra/bin:$PATH"
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 # Set KUBECONFIG
-export KUBECONFIG="$(pwd)/.kcp/admin.kubeconfig"
+export PATH=$PATH:/home/mysterion/Dokumente/BachelorThesis_Infra/contrib-tmc/bin
 
-# Create the workspace
+# Create the workspaces and enter dashboard workspace
 kubectl ws use :root
-kubectl create-workspace dashboard
-kubectl ws use dashboard
-echo "✅ Workspace 'dashboard' created and entered."
-echo "Current workspace: $(kubectl ws current)"
-
-# Create child workspace "cluster"
+kubectl tmc workspace create dashboard --type tmc
+kubectl create-workspace tenants --type universal
 kubectl ws use :root:dashboard
-kubectl apply -f k8s/workspaces/root-dashboard-cluster.yaml
 
-# :root RBAC
-kubectl ws use :root
-kubectl apply -f k8s/rbac/root.yaml
+# Create the kind cluster for the dashboard
+kind create cluster --config ./k8s/cluster/kind-config.yaml
 
-# :root:dashboard binding
-kubectl ws use :root:dashboard
-kubectl kcp bind apiexport root:tenancy.kcp.io --name tenancy
+# Build dashboardBE docker image
+./scripts/build/dashboardBE.sh
 
-# :root:dashboard:cluster binding
-kubectl ws use :root:dashboard:cluster
-kubectl delete apibinding kubernetes --ignore-not-found
-kubectl kcp bind apiexport root:kubernetes --name kubernetes --accept-permission-claim namespaces.core
+# Load image to kind cluster
+kind load docker-image dashboard-be:latest --name dashboard
+
+# Apply deployment
+kubectl config use-context kind-dashboard
+kubectl apply -f ./k8s/deployments/dashboard-be.yaml
+
+# Build dashboardFE docker image
+./scripts/build/dashboardFE.sh http://api.dashboard.local:8080
+
+# Load image to kind cluster
+kind load docker-image dashboard-fe:latest --name dashboard
+
+# Apply deployment
+kubectl apply -f ./k8s/deployments/dashboard-fe.yaml
+
+# Ingress
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/kind/deploy.yaml
+kubectl label node dashboard-control-plane ingress-ready=true
+kubectl wait --namespace ingress-nginx \
+    --for=condition=Ready pod \
+    --selector=app.kubernetes.io/component=controller \
+    --timeout=90s
+sudo bash ./scripts/add-to-hosts.sh 127.0.0.1 dashboard.local api.dashboard.local
+kubectl apply -f ./k8s/ingress/dashboard-ingress.yaml
 
