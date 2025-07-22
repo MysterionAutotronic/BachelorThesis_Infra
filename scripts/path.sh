fd92aa982fbd0e564aad8f5c5da96097c8f6dc33
David Linhardt <divad2012@web.de>
2025-07-22 16:38:44 +0200

    modify path to use right admin.kubeconfig

--- scripts/path.sh
+++ scripts/path.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PATH="$HOME/Dokumente/BachelorThesis_Infra/bin:$PATH"
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-export KUBECONFIG=$(pwd)/.kcp/admin.kubeconfig
+export KUBECONFIG=./contrib-tmc/.tmc/admin.kubeconfig
+export PATH=$PATH:/home/mysterion/Dokumente/BachelorThesis_Infra/contrib-tmc/bin
