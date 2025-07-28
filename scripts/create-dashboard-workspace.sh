a71b9d9cc1b0aa442c3429a298680aec6592892a
David Linhardt <divad2012@web.de>
2025-07-28 12:36:51 +0200

    correct proxy endpoint

--- scripts/create-dashboard-workspace.sh
+++ scripts/create-dashboard-workspace.sh
@@ -37,13 +37,13 @@ kind load docker-image dashboard-fe:latest --name dashboard
 # Apply deployment
 kubectl apply -f ./k8s/deployments/dashboard-fe.yaml
 
-# Ingress
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/kind/deploy.yaml
-kubectl label node dashboard-control-plane ingress-ready=true
-kubectl wait --namespace ingress-nginx \
-    --for=condition=Ready pod \
-    --selector=app.kubernetes.io/component=controller \
-    --timeout=90s
-sudo bash ./scripts/add-to-hosts.sh 127.0.0.1 dashboard.local api.dashboard.local
-kubectl apply -f ./k8s/ingress/dashboard-ingress.yaml
-
+# Ingress (not needed for development, but useful for production)
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/kind/deploy.yaml
+# kubectl label node dashboard-control-plane ingress-ready=true
+# kubectl wait --namespace ingress-nginx \
+    # --for=condition=Ready pod \
+    # --selector=app.kubernetes.io/component=controller \
+    # --timeout=90s
+# sudo bash ./scripts/add-to-hosts.sh 127.0.0.1 dashboard.local api.dashboard.local
+# kubectl apply -f ./k8s/ingress/dashboard-ingress.yaml
+# sudo iptables -t nat -A OUTPUT -p tcp --dport 80 -d 127.0.0.1 -j REDIRECT --to-port 30863
