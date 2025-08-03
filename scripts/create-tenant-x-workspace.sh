992fd75f9d5474b385e57ce0ad29bd26554b33f2
David Linhardt <divad2012@web.de>
2025-08-03 21:34:53 +0200

    add create-tenant-x-workspace script

---
+++ scripts/create-tenant-x-workspace.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# TENANT ID
+TENANT_ID="${1}"
+
+# Check if TENANT_ID is provided
+if [ -z "$TENANT_ID" ]; then
+    echo "❌ Tenant ID is required as the first argument. Usage: ./scripts/create-tenant-x-workspace.sh <TENANT_ID>"
+    exit 1
+fi
+
+# Make sure kubectl + plugins are in PATH
+export PATH="$HOME/Dokumente/BachelorThesis_Infra/bin:$PATH"
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+# Set KUBECONFIG
+export PATH=$PATH:/home/mysterion/Dokumente/BachelorThesis_Infra/contrib-tmc/bin
+
+kubectl config use-context root
+
+# Navigate to the root workspace
+kubectl ws use :root:tenants
+
+# Create the tenant workspace
+kubectl tmc workspace create "tenant-${TENANT_ID}" --type tmc
+kubectl ws use :root:tenants:tenant-${TENANT_ID}
+
+# Create the kind cluster for the tenant
+./scripts/create-tenant-x-cluster.sh "${TENANT_ID}"
+
+# Build tenantBE docker image
+./scripts/build/tenantBE.sh
+
+# Load image to kind cluster
+kind load docker-image tenant-be:latest --name tenant-${TENANT_ID}
+
+# Apply deployment
+kubectl config use-context kind-tenant-${TENANT_ID}
+kubectl apply -f ./k8s/deployments/tenant-be.yaml
+
+# Build tenantFE docker image
+./scripts/build/tenantFE.sh ${TENANT_ID} http://tenant-be.default.svc.cluster.local
+
+# Load image to kind cluster
+kind load docker-image tenant-fe:latest --name tenant-${TENANT_ID}
+
+# Apply deployment
+kubectl apply -f ./k8s/deployments/tenant-fe.yaml
